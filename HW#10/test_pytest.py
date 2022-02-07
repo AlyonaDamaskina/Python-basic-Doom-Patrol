d6155e3720ecc2fc4e7faed4f51e3598c9793697
AlyonaDamaskina <95106571+AlyonaDamaskina@users.noreply.github.com>
2022-02-07 22:51:05 +0200

    Add files via upload

---
+++ HW#10/test_pytest.py
@@ -0,0 +1,22 @@
+import pytest
+import pytest1_functions as fp
+import testing_functions1 as fft
+
+def test_season():
+    assert fp.season('snow') == 'winter'
+    assert fp.season('sun') == 'summer'
+    assert fp.season('asdasdsa') is None
+
+def test_range_list():
+    assert 6 in fp.range_list(5, 10)
+    assert 150 in fp.range_list(10, 200)
+    assert 1500 in fp.range_list(10, 2000)
+
+def test_div():
+    try:
+        fft.div(5, 0)
+    except ZeroDivisionError:
+        assert fft.div(1, 1) == 1
+    assert fft.div(10, 5) == 2
+    assert fft.div(7, 20) == 0.35
+    assert fft.div(21, 2) == 10.5
